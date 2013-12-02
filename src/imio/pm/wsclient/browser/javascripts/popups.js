7614e54e34af929d75a5d9e83b8a958513870ec3
Gauthier Bastien <g.bastien@imio.be>
2013-12-02 13:41:59 +0000

    Added jQuery to be able to turn a button added by imio.actionspanel into a link overlay

--- src/imio/pm/wsclient/browser/javascripts/popups.js
+++ src/imio/pm/wsclient/browser/javascripts/popups.js
@@ -14,6 +14,30 @@ jQuery('a.[id^=plone-contentmenu-actions-plonemeeting_wsclient_action_]').each(f
         closeselector: '[name="form.buttons.cancel"]'
     });}
 });
+
+// while used with imio.actionspanel, we need to turn a <input> into a <a>
+// to be able to use an overlay
+jQuery('input.[class*=apButtonAction_plonemeeting_wsclient_action_]').each(function(){
+    // send an item to PloneMeeting
+    // apply if no proposingGroupId is passed in the request
+    if ($(this).attr('onclick').indexOf('&proposingGroupId=') == -1) {
+    // add a surrounding <a></a> tag as overlays are only working with links
+    // extract window.location attribute from current input.onclick value
+    url = $(this).attr('onclick');
+    $(this).wrap("<a href='"+ url.replace("window.location='", '').replace("'", "") +"'></a>");
+    // remove the onclick and use his value as href for added <a></a>
+    $(this)[0].attributes['onclick'].value = '';
+    // now work with the parent, actually the added <a>
+    parent = $(this).parent();
+    parent.prepOverlay({
+        subtype: 'ajax',
+        formselector: '#form',
+        noform: 'redirect',
+        redirect: $.plonepopups.redirectbasehref,
+        closeselector: '[name="form.buttons.cancel"]'
+    });}
+});
+
 };
 
 jQuery(document).ready(initializeActionsOverlays);
