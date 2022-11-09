f4d4c386e3d4cd3bc6710010d6351f1f7f23fc13
Martin Peeters <martin.peeters@affinitic.be>
2022-11-09 11:18:03 +0100

    Update the locales script to rebuild pot files

--- src/imio/pm/wsclient/locales/sync_pos.sh
+++ src/imio/pm/wsclient/locales/sync_pos.sh
@@ -1,6 +1,8 @@
 files="imio.pm.wsclient PloneMeeting"
 languages="en fr"
 
+i18ndude rebuild-pot --pot imio.pm.wsclient.pot --create imio.pm.wsclient ../
+
 for file in $files; do
     for language in $languages; do
         i18ndude sync --pot $file.pot $language/LC_MESSAGES/$file.po
