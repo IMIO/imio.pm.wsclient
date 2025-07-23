1160e192aeb104effa65555bfcf5bc453658cf05
Wiemboudabous <wiem.boudabous@imio.be>
2025-07-23 10:50:17 +0200

    ix missing values during migration to step 200

--- src/imio/pm/wsclient/upgrade.py
+++ src/imio/pm/wsclient/upgrade.py
@@ -18,7 +18,13 @@ def upgrade_to_200(context):
             "imio.pm.wsclient.browser.settings.IWS4PMClientSettings.pm_url",
             parts[0],
         )
-
+    action_generated = api.portal.get_registry_record("imio.pm.wsclient.browser.settings.IWS4PMClientSettings.generated_actions")
+    for action in action_generated:
+        if action["permissions"] == "SOAP Client Send":
+            action["permissions"] = "WS Client Send"
+        if action["permissions"] == "SOAP Client Access":
+            action["permissions"] = "WS Client Access"
+    api.portal.set_registry_record("imio.pm.wsclient.browser.settings.IWS4PMClientSettings.generated_actions", action_generated)
     setup = api.portal.get_tool("portal_setup")
     setup.runImportStepFromProfile('imio.pm.wsclient:default', 'rolemap')
 
